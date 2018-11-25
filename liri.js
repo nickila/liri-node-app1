e6c66a8a25cf2c5743d8ba750c7d4c5f909fc0ea
Brian Nickila <brian.nickila@gmail.com>
2018-11-24 22:08:44 -0600

    Added movie-this functionality. It all works including the displaying Mr. Nobody when nothing is entered.

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 var Spotify = require('node-spotify-api');
 var axios = require("axios");
-var moment = require ("moment")
+var moment = require("moment")
 
 var spotify = new Spotify({
     id: process.env.SPOTIFY_ID,
@@ -17,35 +17,35 @@ var input = process.argv.slice(3).join(" ");
 
 if (action === "concert-this") {
 
-axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
-.then(function(response) {
-console.log("");
-for (var i = 0; i < 5; i++) {
-  console.log(response.data[i].venue.name);
-  console.log((response.data[i].venue.city) + ", " + (response.data[i].venue.region));
-  console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
-  console.log("");
-}
-})
-.catch(function(error) {
-  if (error.response) {
-    // The request was made and the server responded with a status code
-    // that falls out of the range of 2xx
-    console.log(error.response.data);
-    console.log(error.response.status);
-    console.log(error.response.headers);
-  } else if (error.request) {
-    // The request was made but no response was received
-    // `error.request` is an object that comes back with details pertaining to the error that occurred.
-    console.log(error.request);
-  } else {
-    // Something happened in setting up the request that triggered an Error
-    console.log("Error", error.message);
-  }
-  console.log(error.config);
-});
+    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            console.log("");
+            for (var i = 0; i < 5; i++) {
+                console.log(response.data[i].venue.name);
+                console.log((response.data[i].venue.city) + ", " + (response.data[i].venue.region));
+                console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                console.log("");
+            }
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 
-    
+
 
 } else if (action === "spotify-this-song" && input) {
     spotify.search({ type: 'track', query: input, limit: 1 }, function (err, data) {
@@ -54,16 +54,16 @@ for (var i = 0; i < 5; i++) {
         }
         var songPath = data.tracks.items[0]
         console.log("");
-            console.log(songPath.album.artists[0].name)
-            console.log(songPath.name)
-            if (songPath.preview_url) {
-                console.log(songPath.preview_url)
-            } else if (songPath.external_urls.spotify) {
-                console.log(songPath.external_urls.spotify)
-            } else {
-                console.log("Sorry, no preview available.")
-            }
-            console.log(songPath.album.name)
+        console.log(songPath.album.artists[0].name)
+        console.log(songPath.name)
+        if (songPath.preview_url) {
+            console.log(songPath.preview_url)
+        } else if (songPath.external_urls.spotify) {
+            console.log(songPath.external_urls.spotify)
+        } else {
+            console.log("Sorry, no preview available.")
+        }
+        console.log(songPath.album.name)
     });
 
 } else if (action === "spotify-this-song" && !input) {
@@ -73,19 +73,46 @@ for (var i = 0; i < 5; i++) {
         }
         var songPath = data.tracks.items[0]
         console.log("");
-            console.log(songPath.album.artists[0].name)
-            console.log(songPath.name)
-            if (songPath.preview_url) {
-                console.log(songPath.preview_url)
-            } else if (songPath.external_urls.spotify) {
-                console.log(songPath.external_urls.spotify)
-            } else {
-                console.log("Sorry, no preview available.")
-            }
-            console.log(songPath.album.name)
+        console.log(songPath.album.artists[0].name)
+        console.log(songPath.name)
+        if (songPath.preview_url) {
+            console.log(songPath.preview_url)
+        } else if (songPath.external_urls.spotify) {
+            console.log(songPath.external_urls.spotify)
+        } else {
+            console.log("Sorry, no preview available.")
+        }
+        console.log(songPath.album.name)
     });
 
-} else if (action === "movie-this") {
+} else if (action === "movie-this" && input) {
+    axios.get("https://www.omdbapi.com/?apikey=trilogy&t=" + input)
+        .then(function (response) {
+            console.log("");
+            console.log(response.data.Title)
+            console.log(response.data.Year)
+            console.log("IMDB Rating: " + response.data.imdbRating)
+            console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value)
+            console.log("Made in " + response.data.Country)
+            console.log("Language: " + response.data.Language)
+            console.log("Plot: " + response.data.Plot)
+            console.log("Starring: " + response.data.Actors)
+            console.log("");
+        });
+} else if (action === "movie-this" && !input) {
+    axios.get("https://www.omdbapi.com/?apikey=trilogy&t=mr+nobody")
+        .then(function (response) {
+            console.log("");
+            console.log(response.data.Title)
+            console.log(response.data.Year)
+            console.log("IMDB Rating: " + response.data.imdbRating)
+            console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value)
+            console.log("Made in " + response.data.Country)
+            console.log("Language: " + response.data.Language)
+            console.log("Plot: " + response.data.Plot)
+            console.log("Starring: " + response.data.Actors)
+            console.log("");
+        });
 
 } else if (action === "do-what-it-says") {
 
@@ -105,35 +132,5 @@ for (var i = 0; i < 5; i++) {
 
 
 
-// var inquirer = require("inquirer");
-// inquirer.prompt([
-//         {
-//         type: "input",
-//         message: "Type a band and I'll tell you the genre!",
-//         name: "band"
-//         }
-//     ])
-//     .then(function(inquirerResponse) {
-
-//             console.log("You like " + inquirerResponse.band);
-
-
-//             spotify.search({ type: 'artist', query: inquirerResponse.band, limit: 1 }, function(err, data) {
-//                 if (err) {
-//                   return console.log('Error occurred: ' + err);
-//                 }
-//                 console.log(JSON.stringify(data));
-
-//                 var genre = data.artists.items[0].genres;
-
-
-//               for (i = 0; i < genre.length; i ++) {
-
-//               console.log(JSON.stringify(genre[i]).replace(/['"]+/g, '')); 
-//               }
-//               });
-
-
-//     })
 
 
