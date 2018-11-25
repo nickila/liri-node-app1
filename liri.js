14f1c276922c7b55d337f7c84fb98264f4088937
Brian Nickila <brian.nickila@gmail.com>
2018-11-25 10:05:00 -0600

    Do what it says is working now. I'm able to read and interpret from random.txt file. I can also append to it, but it's commented out for now. I need to set it up so that I can read different parts of the text file rather than the first two indexes.

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 var Spotify = require('node-spotify-api');
 var axios = require("axios");
 var moment = require("moment")
-
+var fs = require('fs');
 var spotify = new Spotify({
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
@@ -13,124 +13,133 @@ var input = process.argv.slice(3).join(" ");
 
 
 
+function liriDo() {
+    if (action === "concert-this") {
 
-
-if (action === "concert-this") {
-
-    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
-        .then(function (response) {
-            console.log("");
-            for (var i = 0; i < 5; i++) {
-                console.log(response.data[i].venue.name);
-                console.log((response.data[i].venue.city) + ", " + (response.data[i].venue.region));
-                console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
+        axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+            .then(function (response) {
                 console.log("");
+                for (var i = 0; i < 5; i++) {
+                    console.log(response.data[i].venue.name);
+                    console.log((response.data[i].venue.city) + ", " + (response.data[i].venue.region));
+                    console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                    console.log("");
+                }
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+
+
+
+    } else if (action === "spotify-this-song" && input) {
+        spotify.search({ type: 'track', query: input, limit: 1 }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
             }
-        })
-        .catch(function (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
+            var songPath = data.tracks.items[0]
+            console.log("");
+            console.log(songPath.album.artists[0].name)
+            console.log(songPath.name)
+            if (songPath.preview_url) {
+                console.log(songPath.preview_url)
+            } else if (songPath.external_urls.spotify) {
+                console.log(songPath.external_urls.spotify)
             } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
+                console.log("Sorry, no preview available.")
             }
-            console.log(error.config);
+            console.log(songPath.album.name)
+            console.log("")
         });
 
-
-
-} else if (action === "spotify-this-song" && input) {
-    spotify.search({ type: 'track', query: input, limit: 1 }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        var songPath = data.tracks.items[0]
-        console.log("");
-        console.log(songPath.album.artists[0].name)
-        console.log(songPath.name)
-        if (songPath.preview_url) {
-            console.log(songPath.preview_url)
-        } else if (songPath.external_urls.spotify) {
-            console.log(songPath.external_urls.spotify)
-        } else {
-            console.log("Sorry, no preview available.")
-        }
-        console.log(songPath.album.name)
-    });
-
-} else if (action === "spotify-this-song" && !input) {
-    spotify.search({ type: 'track', query: "The+Sign", limit: 1 }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        var songPath = data.tracks.items[0]
-        console.log("");
-        console.log(songPath.album.artists[0].name)
-        console.log(songPath.name)
-        if (songPath.preview_url) {
-            console.log(songPath.preview_url)
-        } else if (songPath.external_urls.spotify) {
-            console.log(songPath.external_urls.spotify)
-        } else {
-            console.log("Sorry, no preview available.")
-        }
-        console.log(songPath.album.name)
-    });
-
-} else if (action === "movie-this" && input) {
-    axios.get("https://www.omdbapi.com/?apikey=trilogy&t=" + input)
-        .then(function (response) {
+    } else if (action === "spotify-this-song" && !input) {
+        spotify.search({ type: 'track', query: "The+Sign", limit: 1 }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            var songPath = data.tracks.items[0]
             console.log("");
-            console.log(response.data.Title)
-            console.log(response.data.Year)
-            console.log("IMDB Rating: " + response.data.imdbRating)
-            console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value)
-            console.log("Made in " + response.data.Country)
-            console.log("Language: " + response.data.Language)
-            console.log("Plot: " + response.data.Plot)
-            console.log("Starring: " + response.data.Actors)
-            console.log("");
-        });
-} else if (action === "movie-this" && !input) {
-    axios.get("https://www.omdbapi.com/?apikey=trilogy&t=mr+nobody")
-        .then(function (response) {
-            console.log("");
-            console.log(response.data.Title)
-            console.log(response.data.Year)
-            console.log("IMDB Rating: " + response.data.imdbRating)
-            console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value)
-            console.log("Made in " + response.data.Country)
-            console.log("Language: " + response.data.Language)
-            console.log("Plot: " + response.data.Plot)
-            console.log("Starring: " + response.data.Actors)
-            console.log("");
+            console.log(songPath.album.artists[0].name)
+            console.log(songPath.name)
+            if (songPath.preview_url) {
+                console.log(songPath.preview_url)
+            } else if (songPath.external_urls.spotify) {
+                console.log(songPath.external_urls.spotify)
+            } else {
+                console.log("Sorry, no preview available.")
+            }
+            console.log(songPath.album.name)
         });
 
-} else if (action === "do-what-it-says") {
+    } else if (action === "movie-this" && input) {
+        axios.get("https://www.omdbapi.com/?apikey=trilogy&t=" + input)
+            .then(function (response) {
+                console.log("");
+                console.log(response.data.Title)
+                console.log(response.data.Year)
+                console.log("IMDB Rating: " + response.data.imdbRating)
+                if (response.data.Ratings[1] && response.data.Ratings[1].Source === "Rotten Tomatoes") {
+                    console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value)
+                } else {
+                    console.log("Rotten Tomatoes: N/A")
+                }
+                console.log("Made in " + response.data.Country)
+                console.log("Language: " + response.data.Language)
+                console.log("Plot: " + response.data.Plot)
+                console.log("Starring: " + response.data.Actors)
+                console.log("");
+            });
+    } else if (action === "movie-this" && !input) {
+        axios.get("https://www.omdbapi.com/?apikey=trilogy&t=mr+nobody")
+            .then(function (response) {
+                console.log("");
 
-} else {
+                console.log(response.data.Title)
+                console.log(response.data.Year)
+                console.log("IMDB Rating: " + response.data.imdbRating)
+                console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value)
+                console.log("Made in " + response.data.Country)
+                console.log("Language: " + response.data.Language)
+                console.log("Plot: " + response.data.Plot)
+                console.log("Starring: " + response.data.Actors)
+                console.log("");
+            });
+    }
 
 
-}
+    if (action === "do-what-it-says") {
 
 
+        fs.readFile("random.txt", "utf-8", function read(err, data) {
+            if (err) {
+                throw err;
+            }
+            content = data;
+            processFile();
+        });
 
+        function processFile() {
+            var textArr = content.split(",");
+            action = textArr[0]
+            input = textArr[1]
+            liriDo();
+        }
+    };
+};
 
-
-
-
-
-
-
-
-
-
+liriDo();
 
