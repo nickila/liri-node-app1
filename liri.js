02742fb54b07016f3afa2933685fb5bff76798a9
Brian Nickila <brian.nickila@gmail.com>
2018-11-28 13:37:00 -0600

    Fixed some formatting to the code.

--- liri.js
+++ liri.js
@@ -11,8 +11,6 @@ var spotify = new Spotify({
 var action = process.argv[2];
 var input = process.argv.slice(3).join(" ");
 
-
-
 function liriDo() {
     if (action === "concert-this") {
         axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
@@ -20,7 +18,8 @@ function liriDo() {
                 console.log("");
                 var movieInfo;
                 for (var i = 0; i < 5; i++) {
-                    movieInfo = ("\n" + (response.data[0].lineup[0]) + "\n" + (response.data[i].venue.name) + "\n" +
+                    movieInfo = ("\n" + (response.data[0].lineup[0]) + "\n" +
+                        (response.data[i].venue.name) + "\n" +
                         (response.data[i].venue.city) + "," +
                         (response.data[i].venue.region) + "\n" +
                         (moment(response.data[i].datetime).format("MM/DD/YYYY")) + "\n");
@@ -51,12 +50,14 @@ function liriDo() {
                 return console.log('Error occurred: ' + err);
             }
             var songPath = data.tracks.items[0]
-            var spotResP = ("\n" + songPath.album.artists[0].name + "\n" + 
-            songPath.name + "\n" + 
-            songPath.preview_url + "\n" + 
-            songPath.album.name + "\n")
-            var spotResF = ("\n" + songPath.album.artists[0].name + "\n" + songPath.name + "\n" + songPath.external_urls.spotify + "\n" + songPath.album.name + "\n")
-            
+            var spotResP = ("\n" + songPath.album.artists[0].name + "\n" +
+                songPath.name + "\n" +
+                songPath.preview_url + "\n" +
+                songPath.album.name + "\n")
+            var spotResF = ("\n" + songPath.album.artists[0].name + "\n" +
+                songPath.name + "\n" + songPath.external_urls.spotify + "\n" +
+                songPath.album.name + "\n")
+
             if (songPath.preview_url) {
                 console.log(spotResP)
                 fs.appendFile("spotify-log.txt", "\n" + action + ", " + input + spotResP, function read(err, data) {
@@ -80,7 +81,8 @@ function liriDo() {
     } else if (action === "spotify-this-song" && !input) {
         spotify.request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
             .then(function (data) {
-                var spotNull = ("\n" + data.artists[0].name + "\n" + data.name + "\n" + data.external_urls.spotify + "\n" + data.album.name + "\n")
+                var spotNull = ("\n" + data.artists[0].name + "\n" + data.name + "\n" +
+                    data.external_urls.spotify + "\n" + data.album.name + "\n")
                 console.log(spotNull);
                 fs.appendFile("spotify-log.txt", "\n" + action + ", (no song entered):" + spotNull, function read(err, data) {
                     if (err) {
@@ -95,15 +97,18 @@ function liriDo() {
     } else if (action === "movie-this" && input) {
         axios.get("https://www.omdbapi.com/?apikey=trilogy&t=" + input)
             .then(function (response) {
-                
-                
+
+
                 // console.log("");
                 // console.log(response.data.Title)
                 // console.log(response.data.Year)
                 // console.log("IMDB Rating: " + response.data.imdbRating)
                 console.log(response.data.Ratings)
                 if (!response.data.Ratings[1] || response.data.Ratings[1].Source !== "Rotten Tomatoes") {
-                    var movieInfo = ("\n" + response.data.Title + "\n" + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\n" + "Rotten Tomatoes: N/A\nMade in " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nStarring: " + response.data.Actors + "\n")
+                    var movieInfo = ("\n" + response.data.Title + "\n" + response.data.Year + "\nIMDB Rating: " +
+                        response.data.imdbRating + "\n" + "Rotten Tomatoes: N/A\nMade in " +
+                        response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " +
+                        response.data.Plot + "\nStarring: " + response.data.Actors + "\n")
                     console.log(movieInfo)
                     fs.appendFile("movie-log.txt", "\n" + action + ", " + input + movieInfo, function read(err, data) {
                         if (err) {
@@ -111,7 +116,11 @@ function liriDo() {
                         }
                     })
                 } else if (response.data.Ratings[1] && response.data.Ratings[1].Source === "Rotten Tomatoes") {
-                    var movieInfo = ("\n" + response.data.Title + "\n" + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\n" + response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value + "\nMade in " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nStarring: " + response.data.Actors + "\n")
+                    var movieInfo = ("\n" + response.data.Title + "\n" + response.data.Year + "\nIMDB Rating: " +
+                        response.data.imdbRating + "\n" + response.data.Ratings[1].Source + ": " +
+                        response.data.Ratings[1].Value + "\nMade in " + response.data.Country + "\nLanguage: " +
+                        response.data.Language + "\nPlot: " + response.data.Plot + "\nStarring: " +
+                        response.data.Actors + "\n")
                     console.log(movieInfo)
                     fs.appendFile("movie-log.txt", "\n" + action + ", " + input + movieInfo, function read(err, data) {
                         if (err) {
@@ -119,18 +128,13 @@ function liriDo() {
                         }
                     })
                 }
-                // console.log("Made in " + response.data.Country)
-                // console.log("Language: " + response.data.Language)
-                // console.log("Plot: " + response.data.Plot)
-                // console.log("Starring: " + response.data.Actors)
-                // console.log("");
             });
     } else if (action === "movie-this" && !input) {
         axios.get("https://www.omdbapi.com/?apikey=trilogy&t=mr+nobody")
             .then(function (response) {
                 var movieInfo = ("\n" + response.data.Title + "\n" + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes: N/A" + "\nMade in " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nStarring: " + response.data.Actors + "\n")
                 var movieInfoRT = ("\n" + response.data.Title + "\n" + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\n" + response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value + "\nMade in " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nStarring: " + response.data.Actors + "\n")
-                if (response.data.Ratings[1] && response.data.Ratings[1].Source === "Rotten Tomatoes") {  
+                if (response.data.Ratings[1] && response.data.Ratings[1].Source === "Rotten Tomatoes") {
                     console.log(movieInfoRT)
                     fs.appendFile("movie-log.txt", "\n" + action + ", (no movie entered)" + movieInfoRT, function read(err, data) {
                         if (err) {
@@ -145,26 +149,11 @@ function liriDo() {
                         }
                     })
                 }
-
-
-
-                // console.log("");
-                // console.log(response.data.Title)
-                // console.log(response.data.Year)
-                // console.log("IMDB Rating: " + response.data.imdbRating)
-                // console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value)
-                // console.log("Made in " + response.data.Country)
-                // console.log("Language: " + response.data.Language)
-                // console.log("Plot: " + response.data.Plot)
-                // console.log("Starring: " + response.data.Actors)
-                // console.log("");
             });
     }
 
 
     if (action === "do-what-it-says") {
-
-
         fs.readFile("random.txt", "utf-8", function read(err, data) {
             if (err) {
                 throw err;
@@ -183,4 +172,3 @@ function liriDo() {
 };
 
 liriDo();
-
